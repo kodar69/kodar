f19a3ea28fb3d539a6ce4524e7039cd9a773f7d8
Hardik Upadhyay <119848774+HardikUpadhyay007@users.noreply.github.com>
2024-05-04 14:56:21 +0530

    asd

---
+++ twodarray.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare and initialize a 2D array
+        int[][] array = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Print the elements of the 2D array
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+int[][] array = new int[3][3];
